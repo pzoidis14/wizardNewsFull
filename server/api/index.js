3b9c06b0537c98057426ede94cbd6dd1392f6219
Tyler Zoidis <tyler.zoidis@gmail.com>
2019-06-28 15:17:51 -0400

    commented out attempts at SQL template tag

--- server/api/index.js
+++ server/api/index.js
@@ -37,52 +37,10 @@ router.get('/add', (req, res, next) => {
   }
 });
 
-// router.get('/:id', async (req, res, next) => {
-//   try {
-//     const data = await client.query(
-//       baseQuery + ` WHERE posts.id = ${req.params.id}`
-//     );
-//     const post = data.rows[0];
-//     res.send(postDetails(post));
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const { name, title, content } = req.body;
-//     let userData = await client.query(
-//       `SELECT * FROM users WHERE users.name = $1`,
-//       [name]
-//     );
-//     if (!userData.rows.length) {
-//       userData = await client.query(
-//         `INSERT INTO users (name) VALUES ($1) RETURNING *`,
-//         [name]
-//       );
-//     }
-//     const userId = userData.rows[0].id;
-//     const postData = await client.query(
-//       `INSERT INTO posts (userId, title, content) VALUES ($1, $2, $3) RETURNING *`,
-//       [userId, title, content]
-//     );
-//     const postId = postData.rows[0].id;
-//     await client.query(`INSERT INTO upvotes (userId, postId) VALUES ($1, $2)`, [
-//       userId,
-//       postId,
-//     ]);
-//     res.redirect(`/posts/${postId}`);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
 router.get('/:id', async (req, res, next) => {
   try {
-    console.log(`${baseQuery.strings[0]} WHERE posts.id = ${req.params.id}`);
     const data = await client.query(
-      SQL`${baseQuery.strings[0]} WHERE posts.id = ${req.params.id}`
+      baseQuery + ` WHERE posts.id = ${req.params.id}`
     );
     const post = data.rows[0];
     res.send(postDetails(post));
@@ -119,5 +77,47 @@ router.post('/', async (req, res, next) => {
     next(err);
   }
 });
+
+// router.get('/:id', async (req, res, next) => {
+//   try {
+//     console.log(`${baseQuery.strings[0]} WHERE posts.id = ${req.params.id}`);
+//     const data = await client.query(
+//       SQL`${baseQuery.strings[0]} WHERE posts.id = ${req.params.id}`
+//     );
+//     const post = data.rows[0];
+//     res.send(postDetails(post));
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const { name, title, content } = req.body;
+//     let userData = await client.query(
+//       `SELECT * FROM users WHERE users.name = $1`,
+//       [name]
+//     );
+//     if (!userData.rows.length) {
+//       userData = await client.query(
+//         `INSERT INTO users (name) VALUES ($1) RETURNING *`,
+//         [name]
+//       );
+//     }
+//     const userId = userData.rows[0].id;
+//     const postData = await client.query(
+//       `INSERT INTO posts (userId, title, content) VALUES ($1, $2, $3) RETURNING *`,
+//       [userId, title, content]
+//     );
+//     const postId = postData.rows[0].id;
+//     await client.query(`INSERT INTO upvotes (userId, postId) VALUES ($1, $2)`, [
+//       userId,
+//       postId,
+//     ]);
+//     res.redirect(`/posts/${postId}`);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 module.exports = router;
